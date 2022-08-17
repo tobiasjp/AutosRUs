456b958032b6a80897110258218c4e356bed7c0b
Tobias Pratt <tobix@Tobiass-MBP.attlocal.net>
2022-08-16 21:20:05 -0400

    final code

--- Challenge/MechaCarChallenge.R
+++ Challenge/MechaCarChallenge.R
@@ -7,8 +7,8 @@ suspension_coil<- read.csv(file='Suspension_Coil.csv', check.names = F, stringsA
 total_summary<- suspension_coil %>% summarize(Mean=mean(suspension_coil$PSI), Median=median(suspension_coil$PSI), Variance=var(suspension_coil$PSI), SD=sd(suspension_coil$PSI))
 lot_summary<- suspension_coil %>%group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 
-suspension_sample<- suspension_coil %>% sample_n(50)
-t.test(suspension_sample$PSI, mu=mean(suspension_coil$PSI))
+
+t.test(suspension_coil$PSI, mu=mean(suspension_coil$PSI))
 
 lot1_sub<- subset(suspension_coil, Manufacturing_Lot =='Lot1')
 t.test(lot1_sub$PSI, mu=1500)
